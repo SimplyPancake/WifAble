8c786498804aef947d259cdbb31732c8d7c1a833
SimplyPancake <danielduikvis@gmail.com>
2018-12-27 21:18:56 +0100

    new wifi router thingy

--- install_ap.sh
+++ install_ap.sh
@@ -1,117 +1,118 @@
-#!/bin/bash
+#!/bin/sh
 
-SSID="${1:-WifAble-Free}"
-PASSPHRASE="${2:-WifAble}"
-IP_RANGE="${3:-192.168.1}"
-INC="${4:-eth0}"
-WIFI="${5:-wlan0}"
+#https://github.com/Phoenix1747/RouteryPi
 
-
-echo "Setting up your WiFi-Accesspoint on your pi with:"
-echo " SSID: $SSID"
-echo " PASSPHRASE: $PASSPHRASE"
-echo " IP-Address: $IP_RANGE.1"
-echo " IP-Range: $IP_RANGE.0"
-echo " Incomming device: $INC"
-echo " WiFi device: $WIFI"
-
-
-# update os
-#DONT NEED TO UPDATE, PYTHON ALREADY DOES THAT FOR US!
-# apt-get update
-#apt-get -y upgrade
-apt-get -y install hostapd isc-dhcp-server iptables
-
-# check wlan0 available
-
-if ! ifconfig -a | grep "$WIFI"; then
-  echo "$WIFI not found, exiting";
-  exit -1
-fi
-if ! ifconfig -a | grep "$INC"; then
-  echo "$INC not found, exiting";
-  return -1
-fi
-
-# modify dhcp.conf
-sed -i.bak 's/option domain-name/\#option domain-name/g' /etc/dhcp/dhcpd.conf
-sed -i 's/#authoritative;/authoritative;/g' /etc/dhcp/dhcpd.conf
-
-# add ip addresses
-CONF="
-subnet $IP_RANGE.0 netmask 255.255.255.0 {
-  range $IP_RANGE.10 $IP_RANGE.50;
-  option broadcast-address $IP_RANGE.255;
-  option routers $IP_RANGE.1;
-  default-lease-time 600;
-  max-lease-time 7200;
-  option domain-name "local";
-  option domain-name-servers 8.8.8.8, 8.8.4.4;
+# Check for any errors, quit if any
+check_errors() {
+  if ! [ $? = 0 ]
+  then
+    echo "An error occured! Aborting...."
+    exit 1
+  fi
 }
+
+echo "This will transform your Pi into a WiFi Access Point."
+read -p "To begin with the installation type in 'yes': " out
+
+if ! [ "$out" = "yes" ]
+then
+  echo "You did not type in 'yes'. Exiting..."
+  exit 1
+fi
+
+echo "Updating package sources..."
+sudo apt update>/dev/null 2>&1
+check_errors
+
+echo "Installing hostapd and bridge-utils..."
+sudo apt install hostapd bridge-utils -y>/dev/null 2>&1
+check_errors
+echo "Successfully installed!"
+
+# read -p "Please specify your SSID: " ssid
+# read -p "Please specify a password: " pw
+read -p "Please specify your country code, e.g. DE: " cc
+# if [ "$ssid" ] && [ "$pw" ] && [ "$cc" ]
+if [ "$cc" ]
+then
+  a="
+  # Bridge mode
+  bridge=br0
+
+  # Networking interface
+  interface=wlan0
+
+  # WiFi configuration
+  ssid=WifAble
+  channel=1
+  hw_mode=g
+  country_code=$cc
+  ieee80211n=1
+  ieee80211d=1
+  wmm_enabled=1
+
+  # WiFi security
+  auth_algs=1
+  wpa=2
+  wpa_key_mgmt=WPA-PSK
+  rsn_pairwise=CCMP
+  wpa_passphrase=WifAble
+  "
+else
+  # echo "Empty SSID, password or country code! Exiting..."
+  echo "Empty country code! Exiting..."
+  exit 1
+fi
+
+echo "Writing hostapd.conf..."
+sudo sh -c "echo '$a'>/etc/hostapd/hostapd.conf"
+check_errors
+echo "Successfully wrote hostapd.conf!"
+
+read -p "Please specify your wireless interface name: " wi
+read -p "Please specify your ethernet interface name: " ei
+if [ "$wi" ] && [ "$ei" ]
+then
+  a="
+  auto lo
+  iface lo inet loopback
+
+  # Ethernet
+  auto $ei
+  allow-hotplug $ei
+  iface $ei inet manual
+
+  # WiFi
+  auto $wi
+  allow-hotplug $wi
+  iface $wi inet manual
+  wireless-power off
+
+  # Bridge
+  auto br0
+  iface br0 inet dhcp
+  bridge_ports $ei $wi
+  bridge_fd 0
+  bridge_stp off
+  "
+else
+  echo "Empty wireless or ethernet interface name! Exiting..."
+  exit 1
+fi
+
+echo "Writing /etc/network/interfaces..."
+sudo sh -c "echo '$a'>/etc/network/interfaces"
+check_errors
+echo "Successfully wrote /etc/network/interfaces!"
+
+echo "Writing to /etc/default/hostapd..."
+a="
+RUN_DAEMON=yes
+DAEMON_CONF="/etc/hostapd/hostapd.conf"
 "
-echo "$CONF" >> /etc/dhcp/dhcpd.conf
+sudo sh -c "echo '$a'>/etc/default/hostapd"
+check_errors
+echo "Successfully wrote /etc/default/hostapd!"
 
-# set where DHCP runs
-sed -i.bak "s/\(INTERFACES *= *\).*/\1\"$WIFI\"/" /etc/default/isc-dhcp-server
-
-# set static ip address for $WIFI
-INTERF_CONF="
-# interfaces(5) file used by ifup(8) and ifdown(8)
-# Please note that this file is written to be used with dhcpcd
-# For static IP, consult /etc/dhcpcd.conf and 'man dhcpcd.conf'
-# Include files from /etc/network/interfaces.d:
-source-directory /etc/network/interfaces.d
-auto lo
-iface lo inet loopback
-iface $INC inet manual
-allow-hotplug $WIFI
-iface $WIFI inet static
-  address $IP_RANGE.1
-  netmask 255.255.255.0
-"
-echo "$INTERF_CONF" > /etc/network/interfaces
-ifconfig $WLAN $IP_RANGE.1
-
-# setup hostapd.conf
-CONF_HOST="interface=$WIFI
-ssid=$SSID
-hw_mode=g
-channel=6
-macaddr_acl=0
-auth_algs=1
-ignore_broadcast_ssid=0
-wpa=2
-wpa_passphrase=$PASSPHRASE
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=TKIP
-rsn_pairwise=CCMP"
-echo "$CONF_HOST" > /etc/hostapd/hostapd.conf
-# replace SSID and Passphrase
-
-# deamon config
-sed -i.bak 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
-
-# add ip-forward=1
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-# add iptables
-iptables -t nat -F
-iptables -F
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i $INC -o $WIFI -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i $WIFI -o $INC -j ACCEPT
-# save iptables
-iptables-save > /etc/iptables.ipv4.nat
-
-# load on boot
-echo '#!/bin/sh' > /etc/network/if-up.d/iptables
-echo "echo 'RUNNING iptables restore now'" >> /etc/network/if-up.d/iptables
-echo "iptables-restore < /etc/iptables.ipv4.nat" >> /etc/network/if-up.d/iptables
-echo "exit 0;" >> /etc/network/if-up.d/iptables
-
-chmod +x /etc/network/if-up.d/iptables
-
-# test access point
-echo "Installation done!"
-/usr/sbin/hostapd /etc/hostapd/hostapd.conf &
+echo "Installation is complete. You will want to restart your Pi to make this work."
+echo "No further action should be required. Closing..."
